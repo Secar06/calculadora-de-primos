646f383c0f205c35027773f5865faea68b180a65
Sebas <sebastian.cardona7@udea.edu.co>
2025-09-18 16:52:59 -0500

    codigo corregido

--- calculador de primo.py
+++ calculador de primo.py
@@ -1,10 +1,32 @@
-def es_primo(n, divisores = 0,):
-    for i in range(1,n+1):
-        if n % i == 0:
-            divisores += 1    
-    if divisores <= 2:
-        return "Es primo"
-    elif divisores >= 3:
-            return "No es primo"
+import math
+Primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47]
+max_prime = Primes[-1]
 
-print(es_primo(997))
+def prime_calculator(value):
+    n = math.floor(value**(1/2))
+    i = n
+
+    while i > max_prime:
+        is_prime = True
+        for prime in Primes:
+            if i != prime and i % prime == 0:
+                is_prime = False
+                break
+        if is_prime:
+            Primes.append(i)
+        i -= 1
+
+    flag = True
+    for prime in Primes:
+        if value != prime and value % prime == 0:
+            flag = False
+            return flag
+
+    Primes.append(value)
+    return flag
+
+num = 36788
+if prime_calculator(num):
+    print(f"{num} es un numero primo")
+else:
+    print(f"{num} no es un numero primo")
